079ed1cb4895b75ed94064be5eb91a1c20a65267
Amina <amina.laghmari@gmail.com>
2020-12-18 10:00:56 +0100

    code de bootstrap pour le menu navbar

--- projet-blog/src/components/MenuDroit.js
+++ projet-blog/src/components/MenuDroit.js
@@ -9,30 +9,41 @@ import './MenuDroit.css';
 class MenuDroit extends Component{
   
 
-
-       render() {
-        return(       
-            <nav className = "menuDroit">
+  render() {
+    return(   
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
               <div className="logoBorder">
-                <img src ={logo} alt="It is my logo"></img>
-              </div>  
-             <div className="links-container">
-                <NavLink to="/home">
-                  Home
-                </NavLink>
-                <NavLink to="/projects">
-                  Projects
-                </NavLink>
-                <NavLink to="/blog">
-                  Blog
-                </NavLink>
-                </div>
-              </nav>
+                    <img src ={logo} alt="It is my logo"></img>
+              </div>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                  <NavLink className="nav-link " aria-current="page" to="/home">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/projects">
+                    Projects
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/blog">
+                    Blog
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
-            )        
-    }
-  }
+)        
+}
+}
 
 export default MenuDroit;
-
 
